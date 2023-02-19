dfc4763278a1e76bd576b15bdc607d35a832e6d8
Kairyak Artemy <kairyakaa@mos-team.ru>
2023-02-19 08:50:10 +0400

    db connection added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardsModule } from './cards/cards.module';
-import {GraphQLModule} from "@nestjs/graphql";
-import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
-import {join} from "path";
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-  }), CardsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+      connectTimeout: 20000,
+      acquireTimeout: 20000,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    CardsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/cards/card.entity.ts
+++ src/cards/card.entity.ts
@@ -1,16 +1,22 @@
-import {Field, Float, Int, ObjectType} from "@nestjs/graphql";
+import { Field, Float, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Card {
-  @Field(type => Int)
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
   id: number;
 
+  @Column()
   @Field()
   picture: string;
 
+  @Column()
   @Field()
   name: string;
 
-  @Field(type => Float, {nullable: true})
+  @Column({ nullable: true })
+  @Field((type) => Float, { nullable: true })
   price?: number;
 }
