cba2915263ea3b102b626d0cece1f1a6339600fe
artemykairyak <artemykairyak@yandex.com>
2023-03-05 21:08:09 +0400

    added vercel configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,12 @@ import { AppService } from './app.service';
 import { CardsModule } from './cards/cards.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorsModule } from './authors/authors.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -21,15 +21,15 @@ import { AuthModule } from './auth/auth.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      entities: [join(__dirname + '/**/*.entity{.ts,.js}')],
       autoLoadEntities: true,
       connectTimeout: 20000,
       acquireTimeout: 20000,
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: true,
     }),
     CardsModule,
     AuthorsModule,
